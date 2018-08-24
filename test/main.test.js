61d51f640ed67cdca133cd9ec36dacc47ca27748
rogerwschmidt <rogerschmidt@gmail.com>
2018-08-24 11:12:13 -0400

    updated test

--- test/main.test.js
+++ test/main.test.js
@@ -5,37 +5,27 @@ const expect = chai.expect
 chai.use(require('chai-as-promised'))
 
 describe('answerQuestions', function () {
-  it('should answer the questions from a comma separated list of questions', function () {
+  it('should answer the questions from a comma separated list of questions', function (done) {
     const questions = 'Will it rain?,Should I bring an umbrella?,Will I find $10 today?'
     const result = main.answerQuestions(questions)
 
-    return result.then(answers => {
-      expect(answers).to.be.an('array')
-      expect(answers).to.have.lengthOf(3)
-
-      const allStrings = answers.every(answer => typeof answer === 'string')
-      expect(allStrings).to.be.true
-    })
+    expect(result).to.eventually.be.an('array')
+    expect(result).to.eventually.have.lengthOf(3).notify(() => done())
   })
 
-  it('should answer a single question', function () {
+  it('should answer a single question', function (done) {
     const questions = 'Will it rain?'
     const result = main.answerQuestions(questions)
 
-    return result.then(answers => {
-      expect(answers).to.be.an('array')
-      expect(answers).to.have.lengthOf(1)
-
-      const isString = typeof answers[0] === 'string'
-      expect(isString).to.be.true
-    })
+    expect(result).to.eventually.be.an('array')
+    expect(result).to.eventually.have.lengthOf(1).notify(() => done())
   })
 
-  it('should return a single error as an object questions', function () {
+  it('should return a single error as an object questions', function (done) {
     const questions = 'Will it rain?,,Will I find $10 today?'
     const result = main.answerQuestions(questions)
 
     expect(result).to.eventually.be.rejected
-    expect(result).to.eventually.have.property('error')
+    expect(result).to.eventually.have.property('error').notify(() => done())
   })
 })
